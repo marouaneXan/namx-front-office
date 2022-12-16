bd24e65d0e4a824e6c909e1090e137d3c0513908
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-16 09:36:00 +0100

    create first version for Hero component

---
+++ src/Components/Home/Hero.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen relative">
+      <video
+        className="w-full h-full object-cover"
+        // src={busVid}
+        autoPlay
+        loop
+        muted
+      />
+      <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
+        <h1 className="text-3xl md:text-4xl font-bold">First Class Travel</h1>
+        <h2 className="py-4 text-3xl font-bold">Top 1% Locations Worldwide</h2>
+        {/* <Search /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
