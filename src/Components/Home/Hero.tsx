aed8ae9abbce3c2efd80a114c81b3d2e1aa071b2
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-16 10:33:17 +0100

    add style for Hero content

--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -13,8 +13,28 @@ const Hero = () => {
       />
       <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
-        <h1 className="text-3xl md:text-4xl font-bold">First Class Travel</h1>
-        <h2 className="py-4 text-3xl font-bold">Top 1% Locations Worldwide</h2>
+        <h1
+          className="text-3xl md:text-[30px]"
+          style={{
+            fontWeight: "200",
+            lineHeight: "30px",
+            color: "rgb(255,255,255)",
+            fontFamily: "Kanit, sans-serif",
+          }}
+        >
+          NamX, l’unique
+        </h1>
+        <h2
+          className="py-4 text-[50px]"
+          style={{
+            fontWeight: "500",
+            lineHeight: "50px",
+            color: "rgb(255,255,255)",
+            fontFamily: "Kanit, sans-serif",
+          }}
+        >
+          SUV À HYDROGÈNE
+        </h2>
         {/* <Search /> */}
       </div>
     </div>
